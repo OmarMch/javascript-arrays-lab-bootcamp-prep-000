e7c2c7ce4f8e0bce959c212b6c524c0080999813
Omar Ch <mhcomar28@gmail.com>
2018-01-16 08:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendKitten(name) {
   return kittens.push('Ralph');
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift('Bob');
+}
