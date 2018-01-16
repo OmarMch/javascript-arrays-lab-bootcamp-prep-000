ff6555098459739df3b7ce0a7ce9285ddd1f7d60
Omar Ch <mhcomar28@gmail.com>
2018-01-16 08:48:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,7 @@ function appendKitten(name) {
 function prependKitten(name) {
   return ["Arnold", ...kittens];
 }
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length, -1);
+}
